34b44a04b035ef642015bca37711c6fe7829d7db
Hariharan A <hari20002010@gmail.com>
2021-04-02 18:01:15 +0530

    Add files via upload

---
+++ src/HelloWorld.ino
@@ -0,0 +1,38 @@
+
+
+#include <ros.h>
+#include <std_msgs/Float32.h>
+#define ep 2
+#define tp 3
+long distance;
+long duration;
+ros::NodeHandle  nh;
+
+std_msgs::Float32 str_msg;
+ros::Publisher sensor("sensor", &str_msg);
+
+
+void setup()
+{
+  pinMode(tp,OUTPUT);
+  pinMode(ep , INPUT);
+ 
+  nh.initNode();
+  nh.advertise(sensor);
+}
+
+void loop()
+{
+  digitalWrite(tp , LOW);
+  delayMicroseconds(2);
+  digitalWrite(tp, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(tp , LOW);
+  duration = pulseIn(ep , HIGH);
+  distance = duration *0.034/2;
+  
+  str_msg.data = distance;
+  sensor.publish( &str_msg );
+  nh.spinOnce();
+  delay(100);
+}
---
+++ src/sent_go.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32
+
+
+def callback(msg):
+    print msg.data
+    twist = Twist()
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    if 10 >= msg.data >= 0:
+        twist.linear.x = 0
+        twist.angular.z = 0.5
+    elif 20 >= msg.data > 10:
+        twist.linear.x = 0.5
+        twist.angular.z = 0
+    elif 30 >= msg.data > 20:
+        twist.linear.x = 0
+        twist.angular.z = -0.5
+    elif 40 >= msg.data > 30:
+        twist.linear.x = -0.5
+        twist.angular.z = 0
+    else:
+        twist.linear.x = 0
+        twist.angular.z = 0
+
+    pub.publish(twist)
+
+
+if __name__ == "__main__":
+    rospy.init_node('sensor_decode')
+    sub = rospy.Subscriber('sensor', Float32, callback)
+    rospy.spin()
